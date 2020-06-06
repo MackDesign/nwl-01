cd888ca41af8edd4dbab3eea6a1248f787168e7a
Mackdesign <marcio@design2u.com.br>
2020-06-06 19:12:40 -0300

    Projeto NLW#01

---
+++ server/knexfile.ts
@@ -0,0 +1,20 @@
+//import  knex from 'knex'
+import  path from 'path'
+//import { env } from './env'
+
+module.exports = {
+    client: 'sqlite3',
+    connection: {
+        filename: path.resolve(__dirname, 'src', 'database', 'database.sqlite'),
+    },
+     migrations: {
+         directory: path.resolve(__dirname, 'src', 'database', 'migrations')
+     },
+     seeds: {
+        directory: path.resolve(__dirname, 'src', 'database', 'seeds')
+    },
+     useNullAsDefault: true,
+};
+
+
+  
